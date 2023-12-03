d94224ac2985fad5d12dc4cfc1a8ab55c8cb4e6c
timb01337 <Kastl123@gmx.de>
2023-12-03 14:22:26 +0100

    refactored day1 - part 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,40 +1,28 @@
-﻿using System.IO;
-using System;
-
-var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day1\input.txt");
-
-
+﻿var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day1\input.txt");
 var sum = 0;
-
-
 
 foreach (var line in lines)
 {
     var firstDigit = "";
     var lastDigit = "";
+    var firstDigitFound = false;
 
     for (var i = 0; i < line.Length; i++)
     {
         if (char.IsDigit(line[i]))
         {
-            firstDigit = line[i].ToString();
-            break;
-        }
-    }
+            if (!firstDigitFound)
+            {
+                firstDigit = line[i].ToString();
+                firstDigitFound = true;
+            }
 
-
-    for (var i = line.Length - 1; i >= 0; i--)
-    {
-        if (char.IsDigit(line[i]))
-        {
             lastDigit = line[i].ToString();
-            break;
         }
     }
-
-    var lineResult = firstDigit + lastDigit;
-    var lineResultAsNumber = int.Parse(lineResult);
-    sum += lineResultAsNumber;
+    
+    var lineResult = int.Parse(firstDigit + lastDigit);
+    sum += lineResult;
 
 }
 
