b90b181962e473125241d8636d06ef6a6843b062
timb01337 <Kastl123@gmx.de>
2023-12-03 14:15:39 +0100

    finished day 1 part 1 with pretty poor solution

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,9 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.IO;
 using System;
 
+var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day1\input.txt");
 
-var counter = 10;
 
-for(var i = 0; i < counter; i++)
-    Console.WriteLine("Hello, World!");
+var sum = 0;
+
+
+
+foreach (var line in lines)
+{
+    var firstDigit = "";
+    var lastDigit = "";
+
+    for (var i = 0; i < line.Length; i++)
+    {
+        if (char.IsDigit(line[i]))
+        {
+            firstDigit = line[i].ToString();
+            break;
+        }
+    }
+
+
+    for (var i = line.Length - 1; i >= 0; i--)
+    {
+        if (char.IsDigit(line[i]))
+        {
+            lastDigit = line[i].ToString();
+            break;
+        }
+    }
+
+    var lineResult = firstDigit + lastDigit;
+    var lineResultAsNumber = int.Parse(lineResult);
+    sum += lineResultAsNumber;
+
+}
+
+Console.WriteLine(sum);
