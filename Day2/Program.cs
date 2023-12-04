6c6d58d4e5f7192d0913aadf93a76e17c5f7ae08
timb01337 <Kastl123@gmx.de>
2023-12-04 21:49:15 +0100

    finished day 2 part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,51 +1,34 @@
 ﻿var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day2\input.txt");
 
-
 var result = 0;
 
 foreach (var line in lines)
 {
-   
-
-    var gameId = int.Parse(line.Split(":")[0][5..]);
-    var gameFailed = false;
-
     var game = line.Split(":")[1].Split(";");
-
-    var countRedCubes = 12;
-    var countGreenCubes = 13;
-    var countBlueCubes = 14;
+    int maxRedsInGame = 0, maxGreensInGame = 0, maxBluesInGame = 0;
     
     foreach (var draw in game)
     {
-     
-        
         var reds = ParseDraw(draw.Split(","), "red");
+        if (reds > maxRedsInGame)
+            maxRedsInGame = reds;
+        
         var greens = ParseDraw(draw.Split(","), "green");
-        var blue = ParseDraw(draw.Split(","), "blue");
-
-        countRedCubes -= reds;
-        countGreenCubes -= greens;
-        countBlueCubes -= blue;
-
-        if (countRedCubes < 0 || countGreenCubes < 0 || countBlueCubes < 0)
-        {
-            gameFailed = true;
-            break;
-        }
+        if (greens > maxGreensInGame)
+            maxGreensInGame = greens;
+        
+        var blues = ParseDraw(draw.Split(","), "blue");
+        if (blues > maxBluesInGame)
+            maxBluesInGame = blues;
     }
-
-    if (!gameFailed)
-        result += gameId;
+    
+    result += (maxRedsInGame * maxGreensInGame * maxBluesInGame);
 }
 
 Console.WriteLine(result);
 
-
 int ParseDraw(IEnumerable<string> draw, string color)
 {
-    var ret = int.TryParse(draw.FirstOrDefault(x => x.Contains(color))?.Replace(" " + color, ""), out var result)
-        ? result
-        : 0;
+    var ret = int.TryParse(draw.FirstOrDefault(x => x.Contains(color))?.Replace(" " + color, ""), out var result) ? result : 0;
     return ret;
 }
