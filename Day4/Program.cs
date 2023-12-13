164b0677a9a1af2ec83bdef5e00b942a047b3108
timb01337 <Kastl123@gmx.de>
2023-12-13 20:13:36 +0100

    day 4 part 2 attempt with recursion

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,17 +1,46 @@
-﻿var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day4\input.txt").ToList();
+﻿var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day4\easyInput.txt").ToList();
 
-double sum = 0;
-foreach (var line in lines)
+int sum = 0;
+
+//key = card number
+//value = amount one by card
+var dict = new Dictionary<int, int>();
+
+for (var i = 0; i < lines.Count; i++)
 {
-   var winningNumbers = line.Split(": ")[1].Split(" | ")[0].Trim().Split(' ').Where(x => x != string.Empty).Select(x => int.Parse(x)).ToArray();
-   var numbersOnCard = line.Split(": ")[1].Split(" | ")[1].Trim().Split(' ').Where(x => x != string.Empty).Select(x => int.Parse(x)).ToArray();
-   var countWinningNumbers = numbersOnCard.Intersect(winningNumbers).Count();
-   if (countWinningNumbers == 1)
-       sum += 1;
-   else if (countWinningNumbers == 2)
-       sum += 2;
-   else if (countWinningNumbers > 2)
-       sum += Math.Pow(2,countWinningNumbers - 1);
+    dict.Add(i + 1, CountWinningNumbers(GetWinningNumbers(lines[i]), GetNumbersOnCard(lines[i])));
+}
+
+
+for (var i = 0; i < lines.Count; i++)
+{
+    sum++;
+    CardWinningRecursion(i, CountWinningNumbers(GetWinningNumbers(lines[i]), GetNumbersOnCard(lines[i])));
 }
 
 Console.WriteLine(sum);
+return;
+
+
+void CardWinningRecursion(int currentLine, int cardsWon)
+{
+    for (var i = 1; i <= cardsWon; i++)
+    {
+        Console.WriteLine(currentLine + i);
+        var amountWon = dict[currentLine + i];
+
+        sum += amountWon;
+        if (amountWon == 0)
+            break;
+
+        CardWinningRecursion(currentLine + 1, amountWon);
+    }
+}
+
+int CountWinningNumbers(IEnumerable<int> a, IEnumerable<int> b) => a.Intersect(b).Count();
+
+int[] GetWinningNumbers(string line) => line.Split(": ")[1].Split(" | ")[0].Trim().Split(' ')
+    .Where(x => x != string.Empty).Select(x => int.Parse(x)).ToArray();
+
+int[] GetNumbersOnCard(string line) => line.Split(": ")[1].Split(" | ")[1].Trim().Split(' ')
+    .Where(x => x != string.Empty).Select(x => int.Parse(x)).ToArray();
