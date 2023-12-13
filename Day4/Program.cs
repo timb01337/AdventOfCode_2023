a1e2a38019404c2ec54b459e7120f13fda58884b
timb01337 <Kastl123@gmx.de>
2023-12-13 21:24:40 +0100

    intermediate commit cauze stupid

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,41 +1,55 @@
 ﻿var lines = File.ReadLines(@"E:\Dev\AdventOfCode_2023\Day4\easyInput.txt").ToList();
 
-int sum = 0;
+//Idee: die karte 1 gewinnt jedes mal die karten 1 - 5
+//-> die karte 2 gewinnt jedes mal die karten 3 und 4
+
+
 
 //key = card number
 //value = amount one by card
-var dict = new Dictionary<int, int>();
+var singleCardValue = new Dictionary<int, int>();
 
 for (var i = 0; i < lines.Count; i++)
+    singleCardValue.Add(i + 1, CountWinningNumbers(GetWinningNumbers(lines[i]), GetNumbersOnCard(lines[i])));
+
+//populate the dictionary
+var cardCopyCountDict = singleCardValue.ToDictionary(entry => entry.Key, entry => 1);
+
+foreach (var entry in singleCardValue)
 {
-    dict.Add(i + 1, CountWinningNumbers(GetWinningNumbers(lines[i]), GetNumbersOnCard(lines[i])));
-}
+    //check how many cards we won
+    var amountOfCardsWon = singleCardValue[entry.Key];
 
-
-for (var i = 0; i < lines.Count; i++)
-{
-    sum++;
-    CardWinningRecursion(i, CountWinningNumbers(GetWinningNumbers(lines[i]), GetNumbersOnCard(lines[i])));
-}
-
-Console.WriteLine(sum);
-return;
-
-
-void CardWinningRecursion(int currentLine, int cardsWon)
-{
-    for (var i = 1; i <= cardsWon; i++)
+    if (amountOfCardsWon > 0)
     {
-        Console.WriteLine(currentLine + i);
-        var amountWon = dict[currentLine + i];
+        //the next cards index where we start iterating
+        var nextIndex = entry.Key + 1;
 
-        sum += amountWon;
-        if (amountWon == 0)
-            break;
+        Console.WriteLine($"We are now looking at card {entry.Key}");
+        
+        //the next X cards (amountOfCardsWon) we need to count the copies
+        for (var i = nextIndex; i < nextIndex + amountOfCardsWon; i++)
+        {
+            Console.WriteLine($"We added one copy to Card {i}");
+            cardCopyCountDict[i] += 1;
+        }
 
-        CardWinningRecursion(currentLine + 1, amountWon);
+        Console.WriteLine("-------------");
     }
 }
+
+// you end up with 1 instance of card 1
+// 2 instances of card 2
+// 4 instances of card 3
+// 8 instances of card 4
+// 14 instances of card 5
+// and 1 instance of card 6
+
+var sum = cardCopyCountDict.Sum(entry => singleCardValue[entry.Key] * entry.Value);
+
+Console.WriteLine($"Ich zähle die kopien, denn ich bin ein Kopierer: {sum}");
+return;
+
 
 int CountWinningNumbers(IEnumerable<int> a, IEnumerable<int> b) => a.Intersect(b).Count();
 
