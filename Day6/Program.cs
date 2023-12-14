79734fb50ee118f750d3d2b6b365fee14a734e0a
timb01337 <Kastl123@gmx.de>
2023-12-14 14:30:41 +0100

    finished day 6 part 1 and 2

---
+++ Day6/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = new KeyValuePair<long, long>(56717999, 334113513502430);
+var times = new int[56717999];
+long count = 0;
+
+for (var i = 1; i < times.Length; i++)
+{
+    var timeButtonWasPressed = i + 1;
+    var timeLeftAfterButtonIsReleased = input.Key - timeButtonWasPressed;
+    var distanceTravelled = timeLeftAfterButtonIsReleased * timeButtonWasPressed;
+
+    if (distanceTravelled > input.Value)
+        count++;
+}
+
+Console.WriteLine($"The result is {count}");
